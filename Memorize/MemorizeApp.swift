355aa79d1dbc0211ab54e49535cd5bd228392e43
stu <143880582+stu9403133@users.noreply.github.com>
2025-11-05 20:51:44 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by . Stu on 2025/11/5.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
