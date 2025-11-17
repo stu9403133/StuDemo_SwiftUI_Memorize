f47f4d74e781b29e1ff27c269a4cdb4f5cdbbbac
stu <143880582+stu9403133@users.noreply.github.com>
2025-11-17 22:59:16 +0800

    Lecture_1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    let cardContents: [String] = ["👻", "🎃", "🕷", "🦇","😈","👹","☠️","👾","👺","👁️"]
+    
+    @State var cardLimit: Int = 4
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(0..<cardLimit, id: \.self) { index in
+                CardView(content: cardContents[index])
+            }
+            
+            HStack {
+                Button("More Card") {
+                    if cardLimit < cardContents.count {
+                        cardLimit += 1
+                    }
+                    
+                }
+                
+                Spacer()
+                
+                Button("Less Card") {
+                    if cardLimit > 1 {
+                        cardLimit -= 1
+                    }
+                }.foregroundColor(.red)
+            }
+            
         }
         .padding()
+    }
+}
+
+
+struct CardView: View {
+    @State var isFaceUp: Bool = true
+    @State var content: String = "👻"
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                Rectangle().foregroundColor(.orange)
+                Rectangle().stroke(lineWidth: 10)
+                Text(content).font(.largeTitle)
+            } else {
+                Rectangle().foregroundColor(.white)
+                Rectangle().stroke(lineWidth: 10)
+            }
+            
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
