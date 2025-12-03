506013e6c410c29748d6909e59c24a90671ddf0d
stu <143880582+stu9403133@users.noreply.github.com>
2025-12-03 22:18:49 +0800

    Lecture_3
    1. clean code
    2. create viewModel, model file

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by . Stu on 2025/12/3.
+//
+
+import SwiftUI
+
+// 這是viewModel用來溝通 view, model
+class EmojiMemoryGame {
+//    var model: MemoryGame<String>
+}
